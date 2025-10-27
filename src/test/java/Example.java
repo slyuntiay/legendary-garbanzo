acf293ffe83ee789e583055ba61afb48bdc4fc70
Valeriy Zakharov <valerii.zakharov@simbirsoft.com>
2025-10-27 10:59:53 +0300

    init commit

---
+++ src/test/java/Example.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Example {
+
+    @Test
+    public void foo() {
+        Assert.assertTrue("fisting is 500 bucks", true);
+    }
+
+}
