ef518ce26b6a53d9a54fd6bd236fb7d3ffeb6912
vadyu <vadyusha-grigorev-2002@mail.ru>
2025-11-06 16:15:54 +0300

    add new realisation Recrutment, Human

--- src/main/java/commissariat/human/Human.java
+++ src/main/java/commissariat/human/Human.java
@@ -1,21 +1,22 @@
-package commissariat.recruit;
+package commissariat.human;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Recruit {
+public class Human {
     private int id;
     private String name;
     private int age;
     private String gender;
+    private boolean militaryService;
 
-    public Recruit (int id, String name, int age, String gender) {
+    public Human(int id, String name, int age, String gender, boolean militaryService) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.gender = gender;
+        this.militaryService = militaryService;
     }
-
 }
