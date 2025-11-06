a59e02e7457631775b7ccb698511e9eb02ab51fa
vadyu <vadyusha-grigorev-2002@mail.ru>
2025-11-06 16:15:54 +0300

    add new realisation Recrutment, Human

--- src/main/java/commissariat/recruitment/Recruitment.java
+++ src/main/java/commissariat/recruitment/Recruitment.java
@@ -1,5 +1,27 @@
 package commissariat.recruitment;
 
+import commissariat.human.Human;
+
 public class Recruitment {
 
+    public void add(Human human) {
+        if (human.isMilitaryService()) {
+            return;
+        }
+        if (human.getGender().equals("female") || human.getAge() < 16) {
+            return;
+        }
+    }
+
+    public void remove(Human human) {
+
+    }
+
+    public void update(Human human) {
+
+    }
+
+    public void print() {
+
+    }
 }
