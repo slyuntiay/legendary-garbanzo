c282fc229385d6c27cdbeb891fc342f3f9d8432f
vadyu <vadyusha-grigorev-2002@mail.ru>
2025-11-06 16:32:32 +0300

    hyeta

--- src/main/java/commissariat/recruitment/Recruitment.java
+++ src/main/java/commissariat/recruitment/Recruitment.java
@@ -2,16 +2,20 @@ package commissariat.recruitment;
 
 import commissariat.human.Human;
 
-public class Recruitment {
+import java.util.ArrayList;
+import java.util.List;
 
-    public void add(Human human) {
+public class Recruitment {
+List<Human> humans = new ArrayList<Human>();
+
+    public void addRecruit(Human human) {
         if (human.isMilitaryService()) {
             return;
         }
         if (human.getGender().equals("female") || human.getAge() < 16) {
             return;
         }
-        //
+        humans.add(human);
     }
 
     public void remove(Human human) {
