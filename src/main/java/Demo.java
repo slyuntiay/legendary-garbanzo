465cfd04b4a76861c48a3f92970a66ba1d5e2524
LAnat1995 <a.lyusev95@mail.com>
2025-11-07 17:24:05 +0300

    Исправление недочётов

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -3,7 +3,7 @@ import population.Population;
 public class Demo {
     public static void main(String[] args) {
         Population population = new Population();
-        population.addHumans(50);
+        population.addHumans(10);
         System.out.println(population.getHumans());
     }
 }
--- src/main/java/population/Human.java
+++ src/main/java/population/Human.java
@@ -3,8 +3,8 @@ package population;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+//@Getter
+//@Setter
 public class Human {
     private int id;
     private int age;
