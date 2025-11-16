51e0e0d4b75d8c203709e3e3e52975c0ca949abb
vadyu <vadyusha-grigorev-2002@mail.ru>
2025-11-16 14:24:12 +0300

    close new task

---
+++ src/main/java/juniorSolution/sumsOfPrimes/UniqueArrayElement.java
@@ -0,0 +1,24 @@
+package juniorSolution.sumsOfPrimes;
+
+import java.util.HashMap;
+
+public class UniqueArrayElement {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> mapElement = new HashMap<Integer, Integer>();
+        int [] arrayElement = {2,3,4,5,6,7,8,9,10,10,20,30,20,11,25,6346,647,2,1,325,2,3,5,6,77,8};
+        for (int i : arrayElement) {
+            if (!mapElement.containsKey(i)) {
+                mapElement.put(i, 1);
+            }else{
+                mapElement.put(i, mapElement.get(i) + 1);
+            }
+
+           }
+        for(int i : arrayElement){
+            if(mapElement.get(i) == 1) {
+                System.out.print(i + " первый уникальный");
+                break;
+            }
+        }
+    }
+}
