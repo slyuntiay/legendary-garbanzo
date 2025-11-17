a76eae43a42bb7479922f286c7588919d70b9567
LAnat1995 <a.lyusev95@mail.com>
2025-11-17 18:16:58 +0300

    connection to database

---
+++ src/main/java/database/TableCreator.java
@@ -0,0 +1,30 @@
+package database;
+
+import java.sql.*;
+
+public class TableCreator {
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/postgres", "LAnat", "3815");
+            statement = connection.createStatement();
+
+            String str = "CREATE TABLE users (" +
+                    "user_id serial PRIMARY KEY," +
+                    "name varchar(32) NOT NULL," +
+                    "age smallint NOT NULL)";
+            statement.executeUpdate(str);
+
+            statement.close();
+            connection.close();
+
+            System.out.println("Table created successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
