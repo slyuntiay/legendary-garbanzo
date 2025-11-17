5b8b056c1a9c5219acfc6c66768248c65d596564
vadyu <vadyusha-grigorev-2002@mail.ru>
2025-11-17 11:45:50 +0300

    fix solution

--- src/main/java/vaDick/unique/UniqueArrayMapElement.java
+++ src/main/java/vaDick/unique/UniqueArrayMapElement.java
@@ -1,26 +1,33 @@
 package vaDick.unique;
 
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.stream.Collectors;
-//
-//public class UniqueArrayMapElement {
-//    public static void main(String[] args) {
-//        Map<Integer, Integer> mapElement = new HashMap<>();
-//        int[] arrayElement = {2, 3, 4, 5, 6,7};
-//        for (int i : arrayElement) {
-//            if (!mapElement.containsKey(i)) {
-//                mapElement.put(i, 1);
-//            } else {
-//                mapElement.put(i, mapElement.get(i) + 1);
-//            }
-//            if (!mapElement.isEmpty()) {
-//                Map<Integer> mapStream = mapElement.stream()
-//                        .limit(1)
-//                        .collect(Collectors.toMap());
-//                System.out.println(mapStream);
-//            }
-//        }
-//
-//    }
-//}
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UniqueArrayMapElement {
+    public static void main(String[] args) {
+        Map<Integer, Integer> mapElement = new HashMap<>();
+        Set<Integer> setUnique = new LinkedHashSet<>();
+        int[] arrayElement = {1, 3, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 0, 0, 0, 0, 0, 1};
+        for (int i : arrayElement) {
+            if (!mapElement.containsKey(i)) {
+                mapElement.put(i, 1);
+            } else {
+                mapElement.put(i, mapElement.get(i) + 1);
+            }
+        }
+        for (Integer key : mapElement.keySet()) {
+            if (mapElement.get(key) == 1) {
+                setUnique.add(key);
+            }
+        }
+        if (!setUnique.isEmpty()) {
+            Set<Integer> setStream = setUnique.stream()
+                    .limit(1)
+                    .collect(Collectors.toSet());
+            System.out.println(setStream);
+        }
+    }
+}
