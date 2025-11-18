bd253c7b15a60a8d797f1cadbe0d12eb387eff17
LAnat1995 <a.lyusev95@mail.com>
2025-11-18 12:34:53 +0300

    wip

--- src/main/java/vaDick/unique/UniqueArrayMapElement.java
+++ src/main/java/vaDick/unique/UniqueArrayMapElement.java
@@ -6,8 +6,7 @@ import java.util.stream.Stream;
 
 public class UniqueArrayMapElement {
     public static void main(String[] args) {
-        Map<Integer, Integer> mapElement = new LinkedHashMap<>();
-//        int[] arrayElement = {1, 3, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 0, 0, 0, 0, 0, 1};
+        Map<Integer, Boolean> map = new LinkedHashMap<>();
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         int quantity = scanner.nextInt();
@@ -18,17 +17,18 @@ public class UniqueArrayMapElement {
         }
         System.out.println(list);
 
-        mapElement = list.stream()
-                .filter(s -> !mapElement.containsKey(s));
+        for (int i : list) {
+            if (!map.containsKey(i)) {
+                map.put(i, false);
+            } else {
+                map.put(i, true);
+            }
+        }
 
-
-//        for (int i : list) {
-//            if (!mapElement.containsKey(i)) {
-//                mapElement.put(i, 1);
-//            } else {
-//                mapElement.put(i, mapElement.get(i) + 1);
-//            }
-//        }
-
+        for (int i : map.keySet()) {
+            if (!map.get(i)) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
