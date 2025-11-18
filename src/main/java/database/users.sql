bec4115912e2ac18b0e5312ad5e7acab66af9887
LAnat1995 <a.lyusev95@mail.com>
2025-11-18 15:35:59 +0300

    wip

--- src/main/java/database/users.sql
+++ src/main/java/database/users.sql
@@ -1,1 +1,11 @@
-;
+create table users
+(
+    user_id serial
+        primary key,
+    name    varchar(32) not null,
+    age     smallint    not null
+);
+
+alter table users
+    owner to "LAnat";
+
