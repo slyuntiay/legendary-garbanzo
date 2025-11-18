b0dcd1e332fb20739e0d15b8777ec6cd9dfeefa1
vadyu <vadyusha-grigorev-2002@mail.ru>
2025-11-18 16:35:15 +0300

    wip

--- src/main/java/Demo.java
+++
@@ -1,9 +0,0 @@
-import population.Population;
-
-public class Demo {
-    public static void main(String[] args) {
-        Population population = new Population();
-        population.addHumans(10);
-        System.out.println(population.getHumans());
-    }
-}
---
+++ src/main/java/marketplace/entity/Client.java
@@ -0,0 +1,18 @@
+package marketplace.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Client {
+    private int id;
+    private String surname;
+    private String name;
+
+    public Client(int id, String surname, String name) {
+        this.id = id;
+        this.surname = surname;
+        this.name = name;
+    }
+}
