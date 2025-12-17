514f14d839dcb0057863520b92b73f199a051804
LAnat1995 <a.lyusev95@mail.com>
2025-12-17 20:12:19 +0300

    wip

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -1,11 +1,11 @@
-import org.junit.Assert;
-import org.junit.Test;
-
-public class Example {
-
-    @Test
-    public void foo() {
-        Assert.assertTrue("fisting is 300 bucks", true);
-    }
-
-}
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class Example {
+//
+//    @Test
+//    public void foo() {
+//        Assert.assertTrue("fisting is 300 bucks", true);
+//    }
+//
+//}
