32cf7a85ad6e127d2ca7f92884052b70a4d2dbf1
vadyu <vadyusha-grigorev-2002@mail.ru>
2025-12-19 13:35:29 +0300

    wip

--- src/main/java/marketplace/com/example/sweater/Application.java
+++ src/main/java/marketplace/com/example/sweater/Application.java
@@ -2,8 +2,10 @@ package marketplace.com.example.sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+        (exclude = {DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
