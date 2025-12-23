4f572e17d3bc41bc821d81de549ade94cc02e5e7
LAnat1995 <a.lyusev95@mail.com>
2025-12-23 17:12:27 +0300

    Слияние клиентской и продуктовой веток

--- src/main/java/marketplace/controller/clientControler/ClientController.java
+++ src/main/java/marketplace/controller/clientControler/ClientController.java
@@ -5,10 +5,8 @@ import lombok.RequiredArgsConstructor;
 import marketplace.dto.clientDto.CreateClientRequestDto;
 import marketplace.dto.clientDto.CreateClientResponseDto;
 import marketplace.entity.Client;
-import marketplace.entity.Product;
-import marketplace.service.ClientService;
+import marketplace.service.clientService.ClientService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
