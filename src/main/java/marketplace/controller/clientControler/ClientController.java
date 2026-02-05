2dcc455d86c70cb0a14cc93bb94d963921b58265
LAnat1995 <a.lyusev95@mail.com>
2026-02-05 17:10:21 +0300

    Логирование ClientController

--- src/main/java/marketplace/controller/clientControler/ClientController.java
+++ src/main/java/marketplace/controller/clientControler/ClientController.java
@@ -2,6 +2,7 @@ package marketplace.controller.clientControler;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import marketplace.dto.clientDto.ClientRequestDto;
 import marketplace.dto.clientDto.ClientResponseDto;
 import marketplace.entity.Client;
@@ -9,6 +10,7 @@ import marketplace.service.clientService.ClientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "client")
 @RequiredArgsConstructor
@@ -18,31 +20,61 @@ public class ClientController {
     @PostMapping(path = "/save")
     public ResponseEntity<ClientResponseDto> save(
             @RequestBody ClientRequestDto clientRequestDto) {
+        log.info("SAVE client: surname={}, name={}",
+                clientRequestDto.getSurname(), clientRequestDto.getName());
+
         Client client = clientService.save(clientRequestDto);
         ClientResponseDto responseDto = new ClientResponseDto(client);
+        log.info("SAVE OK: clientId={}", client.getId());
+
         return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping(path = "/find/{id}")
     public ResponseEntity<ClientResponseDto> find(@PathVariable int id) {
+        log.info("FIND client by id={}", id);
+
         return clientService.find(id)
-                .map(client -> ResponseEntity.ok(new ClientResponseDto(client)))
-                .orElse(ResponseEntity.notFound().build());
+                .map(client -> {
+                    log.info("FIND OK: clientId={}", client.getId());
+                    return ResponseEntity.ok(new ClientResponseDto(client));
+                })
+                .orElseGet(() -> {
+                    log.warn("FIND NOT FOUND: clientId={}", id);
+                    return ResponseEntity.notFound().build();
+                });
     }
 
     @PutMapping(path = "/merge/{id}")
     public ResponseEntity<ClientResponseDto> merge(
             @PathVariable int id,
             @Valid @RequestBody ClientRequestDto clientRequestDto) {
+        log.info("MERGE client id={}, surname={}, name={}",
+                id, clientRequestDto.getSurname(), clientRequestDto.getName());
+
         return clientService.merge(id, clientRequestDto)
-                .map(client -> ResponseEntity.ok(new ClientResponseDto(client)))
-                .orElse(ResponseEntity.notFound().build());
+                .map(client -> {
+                    log.info("MERGE OK: clientId={}", client.getId());
+                    return ResponseEntity.ok(new ClientResponseDto(client));
+                })
+                .orElseGet(() -> {
+                    log.warn("MERGE NOT FOUND: clientId={}", id);
+                    return ResponseEntity.notFound().build();
+                });
     }
 
     @DeleteMapping(path = "/remove/{id}")
     public ResponseEntity<Object> remove(@PathVariable int id) {
+        log.info("REMOVE client id={}", id);
+
         return clientService.remove(id)
-                .map(deleted -> ResponseEntity.noContent().build())
-                .orElse(ResponseEntity.notFound().build());
+                .map(deleted -> {
+                    log.info("REMOVE OK: clientId={}", id);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> {
+                    log.warn("REMOVE NOT FOUND: clientId={}", id);
+                    return ResponseEntity.notFound().build();
+                });
     }
 }
