14573f39ee84612775be35eae73aa77314459568
LAnat1995 <a.lyusev95@mail.com>
2026-02-05 17:50:15 +0300

    Логирование ClientService

--- src/main/java/marketplace/service/clientService/ClientService.java
+++ src/main/java/marketplace/service/clientService/ClientService.java
@@ -1,6 +1,7 @@
 package marketplace.service.clientService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import marketplace.dto.clientDto.ClientRequestDto;
 import marketplace.entity.Client;
 import marketplace.repository.client.ClientRepo;
@@ -9,35 +10,65 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ClientService{
     private final ClientRepo clientRepo;
 
     @Transactional
-    public Client save(ClientRequestDto clientRequestDto) {
-        Client client = new Client(clientRequestDto);;
-        return clientRepo.save(client);
+    public Client save(ClientRequestDto dto) {
+        log.info("SAVE client: surname={}, name={}",
+                dto.getSurname(), dto.getName());
+
+        try {
+            Client client = new Client(dto);
+            Client saved = clientRepo.save(client);
+            log.info("SAVE OK: clientId={}", saved.getId());
+            return saved;
+        } catch (Exception e) {
+            log.error("SAVE FAILED: surname={}, name={}, error={}",
+                    dto.getSurname(), dto.getName(), e.getMessage(), e);
+            throw e;
+        }
     }
 
     @Transactional(readOnly = true)
     public Optional<Client> find(long id) {
-        return clientRepo.find(id);
+        log.debug("FIND client: id={}", id);
+
+        Optional<Client> client = clientRepo.find(id);
+        if (client.isPresent()) {
+            log.debug("FIND OK: clientId={}", id);
+        } else {
+            log.warn("FIND NOT FOUND: id={}", id);
+        }
+        return client;
     }
 
     @Transactional
     public Optional<Client> merge(long id, ClientRequestDto clientRequestDto) {
+        log.info("MERGE client: id={}, surname={}, name={}",
+                id, clientRequestDto.getSurname(), clientRequestDto.getName());
+
         return clientRepo.find(id).map(client -> {
+            log.debug("MERGE updating client: id={}", id);
             client.setSurname(clientRequestDto.getSurname());
             client.setName(clientRequestDto.getName());
-            return clientRepo.merge(client);
+            Client merged = clientRepo.merge(client);
+            log.info("MERGE OK: clientId={}", merged.getId());
+            return merged;
         });
     }
 
     @Transactional
     public Optional<Boolean> remove(long id) {
+        log.info("REMOVE client: id={}", id);
+
         return clientRepo.find(id).map(client -> {
+            log.debug("REMOVE deleting client: id={}", id);
             clientRepo.remove(client);
+            log.info("REMOVE OK: clientId={}", id);
             return true;
         });
     }
