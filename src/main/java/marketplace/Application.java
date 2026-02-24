7668a18b19981a1ce80e790f3a0c34984f9e7d69
LAnat1995 <a.lyusev95@mail.com>
2026-02-24 15:15:09 +0300

    wip

--- src/main/java/marketplace/Application.java
+++ src/main/java/marketplace/Application.java
@@ -1,11 +1,13 @@
 package marketplace;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableRabbit
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
